92ade6bfe9fb17830115d94a09fbe29330af44b7
Onur <43381986+onursert@users.noreply.github.com>
2022-06-18 18:51:26 +0300

    Update MainActivity.java

--- src/main/java/com/github/onursert/bookpub/MainActivity.java
+++ src/main/java/com/github/onursert/bookpub/MainActivity.java
@@ -304,7 +304,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 sendEmail();
                 break;
             case R.id.nav_contact:
-                Intent intentContact = new Intent(Intent.ACTION_VIEW, Uri.parse("https://onursert.org/"));
+                Intent intentContact = new Intent(Intent.ACTION_VIEW, Uri.parse("https://onursert.com/"));
                 startActivity(intentContact);
                 break;
             /*case R.id.nav_komik:
